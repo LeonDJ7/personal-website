7b3d48b14e79c325889f92647409154b8f7eaf6a
Leon Djusberg <leondjust@me.com>
2021-06-02 16:30:36 -0400

    navbar shrinks properly when window is resized

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,69 +1,23 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import 'antd/dist/antd.css'
-import { Button, Avatar } from 'antd'
-import { UserOutlined } from '@ant-design/icons'
+import { Button, Avatar, Dropdown, Menu } from 'antd'
+import { UserOutlined, MenuOutlined } from '@ant-design/icons'
 import portfolioIcon from '../images/portfolio_icon.png'
-import FlipMove from 'react-flip-move'
 import $ from 'jquery'
-
-const styles = {
-
-    parent: {
-        borderBottom: '5px solid lightcoral',
-        display: 'flex',
-        alignItems: 'center',
-        alignContent: 'center',
-        height: '70px'
-    },
-    header: {
-        //marginTop: '3vh',
-        marginLeft: '5vh',
-        display: 'flex',
-        flexGrow: '1',
-        alignItems: 'center',
-        alignContent: 'center',
-    },
-    headerLabel: {
-        display: 'inline-block',
-        fontSize: '20px',
-        fontFamily: 'Montserrat',
-        fontWeight: '600',
-        marginLeft: '1.5vh',
-        color: 'rosybrown'
-    },
-    options: {
-        float: 'right',
-        marginRight: '7vh',
-    },
-    optionsButton: {
-        fontFamily: 'Montserrat',
-        fontWeight: '600',
-        fontSize: '20px',
-        color: 'dimgrey',
-    },
-    selectedIndicator: {
-        height: '5px',
-        backgroundColor: 'white',
-        position: 'absolute',
-        left: '0',
-        right: '0',
-        bottom: '-18px'
-    }
-
-}
+import './NavBar.css'
 
 const NavBar = (props) => {
 
     const NavHeader = (props) => {
 
         return (
-            <div style={styles['header']}>
+            <div id='header' >
                 <Link to="/personal-website">
                     <Avatar src={portfolioIcon} shape='square' size='large' icon={<UserOutlined />}/>
                 </Link>
                 <Link to="/personal-website">
-                    <span style={styles['headerLabel']}> Leon Djusberg </span>
+                    <span id='headerLabel' > Leon Djusberg </span>
                 </Link>
             </div>
         )
@@ -74,41 +28,80 @@ const NavBar = (props) => {
         const [option, setOption] = React.useState('about')
 
         return (
-            <span style={styles['options']} >
+            <span id='options' >
 
                 <Link to="/personal-website">
-                    <Button id='1' onClick={() => { setOption('about'); console.log($('#1').width()) }} type='text' size='large' style={styles['optionsButton']}>About
-                        { option == 'about' && <div style={styles['selectedIndicator']}></div> }
+                    <Button id='1' onClick={() => { setOption('about'); console.log($('#1').width()) }} type='text' size='large' className='optionsButton'>About
+                        { option === 'about' && <div className='selectedIndicator'></div> }
                     </Button>
                     
                 </Link>
 
                 <Link to="/personal-website/projects">
-                    <Button id='2' onClick={() => { setOption('projects'); console.log($('#2').width()) }} type='text' size='large' style={styles['optionsButton']}>Projects
-                        { option == 'projects' && <div style={styles['selectedIndicator']}></div> }
+                    <Button id='2' onClick={() => { setOption('projects'); console.log($('#2').width()) }} type='text' size='large' className='optionsButton'>Projects
+                        { option === 'projects' && <div className='selectedIndicator'></div> }
                     </Button>
                 </Link>
 
                 <Link to="/personal-website/resume">
-                    <Button id='3' onClick={() => { setOption('resume'); console.log($('#3').width()) }} type='text' size='large' style={styles['optionsButton']}>Resume
-                        { option == 'resume' && <div style={styles['selectedIndicator']}></div> }
+                    <Button id='3' onClick={() => { setOption('resume'); console.log($('#3').width()) }} type='text' size='large' className='optionsButton'>Resume
+                        { option === 'resume' && <div className='selectedIndicator'></div> }
                     </Button>
                 </Link>
 
                 <Link to="/personal-website/contact">
-                    <Button id='4' onClick={() => { setOption('contact'); console.log($('#4').width()) }} type='text' size='large' style={styles['optionsButton']}>Contact
-                        { option == 'contact' && <div style={styles['selectedIndicator']}></div> }
+                    <Button id='4' onClick={() => { setOption('contact'); console.log($('#4').width()) }} type='text' size='large' className='optionsButton'>Contact
+                        { option === 'contact' && <div className='selectedIndicator'></div> }
                     </Button>
                 </Link>
-
                 
             </span>
         )
     }
 
+    const NavOptionsDropdown = (props) => {
+
+        const DropdownMenu = (props) => {
+            
+            return (
+                <Menu >
+                    <Link to="/personal-website">
+                        <Menu.Item {...props} key="1" className='menuButton'>
+                            about
+                        </Menu.Item>
+                    </Link>
+                    <Link to="/personal-website/projects">
+                        <Menu.Item {...props} key="2" className='menuButton'>
+                            projects
+                        </Menu.Item>
+                    </Link>
+                    <Link to="/personal-website/resume">
+                        <Menu.Item {...props} key="3" className='menuButton'>
+                            resume
+                        </Menu.Item>
+                    </Link>
+                    <Link to="/personal-website/contact">
+                        <Menu.Item {...props} key="4" className='menuButton'>
+                            contact
+                        </Menu.Item>
+                    </Link>
+                </Menu>
+            )
+        }
+
+        return (
+            <span id='optionsDropdown' >
+                <Dropdown overlay={DropdownMenu} >
+                    <Button icon={<MenuOutlined/>} />
+                </Dropdown>
+            </span>
+        )
+    }
+
     return (
-        <div style={styles['parent']}>
+        <div id='navContainer' >
             <NavHeader/>
+            <NavOptionsDropdown/>
             <NavOptions/>
         </div>
     )
